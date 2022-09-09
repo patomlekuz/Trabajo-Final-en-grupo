54ae671aa23f497bec42715ea3e945b92462061a
Patricio Mlekuz <patomlekuz2@gmail.com>
2022-09-09 19:44:26 -0300

    login/logout/register

--- concesionarios/forms.py
+++ concesionarios/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ClienteFormulario(forms.Form):
     nombre=forms.CharField(max_length=50)
@@ -17,3 +19,13 @@ class SucursalFormulario(forms.Form):
     localidad=forms.CharField(max_length=50)
     empleados=forms.IntegerField()
     fecha_inaugural=forms.DateField(input_formats=['%Y-%m-%d'])
+
+class UserRegisterForm(UserCreationForm):
+    email= forms.EmailField()
+    password1= forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+    password2= forms.CharField(label="Confirmar contraseña", widget=forms.PasswordInput)
+    
+    class Meta:
+        model= User
+        fields= ["username","email","password1","password2"]
+        help_texts= {k:"" for k in fields}
